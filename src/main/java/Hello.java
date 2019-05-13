9e1e527ce8fb4d882441a6ce665cc8f2a6a0b1dd
15810167107 <49835200+15810167107@users.noreply.github.com>
2019-05-13 16:04:18 +0800

    Update Hello.java
    模拟冲突

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("这世界，我来了");
+        System.out.println("模拟代码冲突");
     }
 }
